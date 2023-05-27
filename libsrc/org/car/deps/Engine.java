68445b90bd08596e49beda979177caf1b991dede
Magesh K <47920326+mahee96@users.noreply.github.com>
2023-05-28 00:25:52 +0530

    added reproducer

---
+++ libsrc/org/car/deps/Engine.java
@@ -0,0 +1,8 @@
+package org.car.deps;
+
+import java.lang.String;
+public class Engine {
+    public Engine(){
+        System.out.println("Engine instantiated");
+    }
+}
---
+++ src/main/java/Car.java
@@ -0,0 +1,9 @@
+
+import org.car.deps.Engine;
+
+public class Car {
+    public static void main(String[] args) {
+        new Engine();
+        System.out.println("Car instantiated");
+    }
+}
